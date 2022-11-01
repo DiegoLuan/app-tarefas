27ad0fe71a2cd247702becb12a6b0d1a6d468b5c
Diego Chaves <dlochaves.00@gmail.com>
2022-11-01 14:10:16 -0300

    alterando efeito de cor no titulo da aplicacao

--- src/App.vue
+++ src/App.vue
@@ -26,12 +26,4 @@ header {
   color: #fff;
   text-shadow: 3px 2px 7px #000000;
 }
-
-header:hover {
-  color: #fff;
-  text-shadow: #fff 0px 0px 5px, #fff 0px 0px 10px, #fff 0px 0px 15px,
-    #fff 0px 0px 20px, #fff 0px 0px 30px, #fff 0px 0px 40px, #fff 0px 0px 50px,
-    #fff 0px 0px 75px, 3px 2px 0px rgba(0, 0, 0, 0);
-  transition: all 1s;
-}
 </style>
